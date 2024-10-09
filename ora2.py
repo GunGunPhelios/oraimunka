80e3b164ae6bad9bd384acd981ddbe2ce7ceea5d
GunGunPhelios <halaszszabodaniel2002@gmail.com>
2024-10-09 14:19:33 +0200

    Add files via upload

--- ora2.py
+++ ora2.py
@@ -14,3 +14,32 @@ print(f"{random.choice(alany)} {random.choice(ige)} {random.choice(targy)}")
 karakterek= string.ascii_letters + string.digits
 jelszó=".".join(random. choice(karakterek) for i in range (8))
 print(jelszó)
+
+
+lista=['kő', 'papír', 'olló',]
+gep= random.choice(lista)
+user= input("válassz egyet: kő, papír, olló:")
+if user == gep:
+    print("Döntetlen")
+elif user == "kő" and gep == "olló" or user =="olló" and gep =="papír" or user == "Papír" and gep == "kő":
+    print("Győztél.")
+else:
+    print("Vesztettél.")
+
+print(f"A gép választotta {gep}, választásod: {user}")
+#
+
+
+szamok= []
+
+while len(szamok) <= 15:
+    szam=random.randint(1,200)
+    if szam % 2 == 0:
+        szamok.append(szam)
+print(f"A lista hossza: {len(szamok)}")
+print(';'. join(map(str,szamok)))
+
+with open("data.txt", "w") as file:
+    file.write(';'. join(map(str,szamok)))
+    for i in range(10):
+        
